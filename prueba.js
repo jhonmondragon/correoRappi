39bb835a592720e492715c072ff76390a49996fb
Jhon Mondragon <jhonalmo23@gmail.com>
2021-04-09 10:27:44 -0500

    Acutalizacion de archivos y templates para el area de fotografia

---
+++ prueba.js
@@ -0,0 +1,5 @@
+GET https://user-api-v2.simplybook.me/admin/calendar?mode=provider&filter[status]=confirmed_pending&filter[date_from]=2020-08-26&filter[date_to]=2020-08-26
+
+Content-Type: application/json
+X-Company-Login: <rappifotografiaco>
+X-Token: <27df0743a5fe96b88c2fb141cee86701967f0041334b4ff287bb0f04d400fb97>
